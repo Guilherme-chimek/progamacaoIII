89dcf61c54d9ef3443bc06929eb41f5ff3c9c897
Guilherme-chimek <guilhermechimek78@gmail.com>
2026-04-10 20:11:00 -0300

    yhjg

---
+++ site_IA/script.js
@@ -0,0 +1,36 @@
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// fecha menu ao clicar
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
+
+// abrir música
+function abrirMusica(url) {
+  window.open(url, "_blank");
+}
+
+/* =========================
+   ANIMAÇÃO AO ROLAR
+========================= */
+
+const elements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+elements.forEach(el => observer.observe(el));
