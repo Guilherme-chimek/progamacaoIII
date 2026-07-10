3184ac7a55b3d8685c139fb22d9eb61c023a01ff
guilherme-chimek1 <guilhermechimek78@gmail.com>
2026-07-10 14:35:48 -0300

    gujk
    ui

---
+++ js/jvascrypt.js
@@ -0,0 +1,8 @@
+let cores =['azul', 'amarelo', 'rosa', 'vermelho'];
+//let cores =['0', '1', '2', '3'];
+//let cores =['azul', 'amarelo', 'rosa', 'vermelho'];
+//push() adiciona itens ao array
+cores.push('roxo', 'ciano');
+//pop() remove elemntos do final do vetor
+cores.pop();
+console.log(cores);
